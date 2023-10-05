f61ac32f6afb99f415529cf6875c4effcc759bf8
Esdras Lima <esdraslimaf@gmail.com>
2023-10-05 20:50:31 -0300

    Refatoração do código da IUserService e UserService para suportarem DTOs automapper

--- Api.Domain/Interfaces/Services/User/IUserService.cs
+++ Api.Domain/Interfaces/Services/User/IUserService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Api.Domain.Dtos.User;
 using Api.Domain.Entities;
 
 namespace Api.Domain.Interfaces.Services.User
@@ -9,10 +10,10 @@ namespace Api.Domain.Interfaces.Services.User
     public interface IUserService
     {
         //No método estou utilizando nomeclaturas que lembrem aos verbos HTTP para ficar mais didático
-        Task<UserEntity> Get(Guid id); //Buscará o usuário
-        Task<IEnumerable<UserEntity>> GetAll(); //Buscará todos os usuários
-        Task<UserEntity> Post(UserEntity user); //Irá adicionar o usuário
-        Task<UserEntity> Put(UserEntity user); //Irá atualizar o usuário
+        Task<UserDto> Get(Guid id); //Buscará o usuário
+        Task<IEnumerable<UserDto>> GetAll(); //Buscará todos os usuários
+        Task<UserDtoCreateResult> Post(UserDto user); //Irá adicionar o usuário
+        Task<UserDtoUpdateResult> Put(UserDto user); //Irá atualizar o usuário
         Task<bool> Delete(Guid id); //Deleta por id
     }
-}
+} 
--- Api.Service/Services/UserService.cs
+++ Api.Service/Services/UserService.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Api.Domain.Dtos.User;
 using Api.Domain.Entities;
 using Api.Domain.Interfaces;
 using Api.Domain.Interfaces.Services.User;
+using Api.Domain.Models;
+using AutoMapper;
 
 namespace Api.Service.Services
 {
@@ -12,38 +15,53 @@ namespace Api.Service.Services
     {
 
         private IRepository<UserEntity> _repository; //Logicamente depois vamos dizer que para IRepository<UserEntity> deverá ser chamado por exemplo o BaseRepository<UserEntity>
-
-        public UserService(IRepository<UserEntity> repository) //Aqui o UserEntity está substituindo o <T> generics do IRepository<T>
+        private readonly IMapper _mapper;
+        public UserService(IRepository<UserEntity> repository, IMapper mapper) //Aqui o UserEntity está substituindo o <T> generics do IRepository<T>
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
- // Aqui na Service poderíamos implementar também as regras de negócios, validações, etc. Exemplo: Supondo que para buscar um UserEntity ele tem que ter Id>=100, 
- // podemos colocar essa regra dentro do método aqui na service para verificar se o user que chegou aqui possui tal requisito /\.
+        // Aqui na Service poderíamos implementar também as regras de negócios, validações, etc. Exemplo: Supondo que para buscar um UserEntity ele tem que ter Id>=100, 
+        // podemos colocar essa regra dentro do método aqui na service para verificar se o user que chegou aqui possui tal requisito /\.
 
         public async Task<bool> Delete(Guid id)
         {    
             return await _repository.DeleteAsync(id); //Await pra aguardar o método do repositório assíncrono ser concluído e receber o bool
         }
 
-        public async Task<UserEntity> Get(Guid id)
+        public async Task<UserDto> Get(Guid id)
         {
-            return await _repository.SelectAsync(id); //Await pra aguardar o método do repositório assíncrono ser concluído e receber o UserEntity
+            //return antes do mapper/dto: return await _repository.SelectAsync(id);
+
+            var entity = await _repository.SelectAsync(id); //Await pra aguardar o método do repositório assíncrono ser concluído e receber o UserEntity
+            return _mapper.Map<UserDto>(entity); //Converte UserEntity para UserDto
         }
 
-        public async Task<IEnumerable<UserEntity>> GetAll()
+        public async Task<IEnumerable<UserDto>> GetAll()
         {
-            return await _repository.SelectAsync(); //Await pra aguardar o método do repositório assíncrono ser concluído e receber o IEnumerable<UserEntity>
+           // return await _repository.SelectAsync(); //Await pra aguardar o método do repositório assíncrono ser concluído e receber o IEnumerable<UserEntity>
+            var entities = await _repository.SelectAsync();
+            return _mapper.Map<IEnumerable<UserDto>>(entities);
         }
 
-        public async Task<UserEntity> Post(UserEntity user)
-        {
-            return await _repository.InsertAsync(user); //Await pra aguardar o método do repositório assíncrono ser concluído e receber o UserEntity
+        public async Task<UserDtoCreateResult> Post(UserDto user)
+        {           
+            // return await _repository.InsertAsync(user); //Await pra aguardar o método do repositório assíncrono ser concluído e receber o UserEntity
+            var model = _mapper.Map<UserModel>(user);
+            var entity = _mapper.Map<UserEntity>(model);
+            var result = await _repository.InsertAsync(entity);
+            return _mapper.Map<UserDtoCreateResult>(result);
+            
         }
 
-        public async Task<UserEntity> Put(UserEntity user)
+        public async Task<UserDtoUpdateResult> Put(UserDto user)
         {
-            return await _repository.UpdateAsync(user); //Await pra aguardar o método do repositório assíncrono ser concluído e receber o UserEntity
+            //return await _repository.UpdateAsync(user); //Await pra aguardar o método do repositório assíncrono ser concluído e receber o UserEntity
+            var model = _mapper.Map<UserModel>(user);
+            var entity = _mapper.Map<UserEntity>(model);
+            var result = await _repository.UpdateAsync(entity);
+            return _mapper.Map<UserDtoUpdateResult>(result);
         }
     }
 }
